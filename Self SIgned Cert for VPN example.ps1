7882800587cc0eb01491e1d1ccb1bd1b365b1ced
JbestAzureExpert <112558094+JbestAzureExpert@users.noreply.github.com>
2022-09-07 15:12:49 -0400

    Add files via upload
    This is an example of how to make a self signed certificate using powershell. This can be used to make a point to site vpn

---
+++ Self SIgned Cert for VPN example.ps1
@@ -0,0 +1,12 @@
+﻿#Create Root Cer
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=P2SRootCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+
+#Create User Cert 
+New-SelfSignedCertificate -Type Custom -DnsName PS2ChildCert -KeySpec Signature `
+-Subject "CN=P2SChildCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "cert:\CurrentUser\My" `
+-Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
