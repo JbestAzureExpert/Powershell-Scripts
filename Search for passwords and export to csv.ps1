177f0c4783db675f5ed0f0aa1cd49addcb5acbf0
Joshua Best <112558094+Joshuatbest@users.noreply.github.com>
2022-09-21 08:19:39 -0400

    Add files via upload

---
+++ Search for passwords and export to csv.ps1
@@ -0,0 +1,6 @@
+﻿$path = 'Examplefilediryouwantsearch'
+$ExportPath = 'Examplefilepathwhere you want your report to be saved to.csv.'
+
+
+
+Get-Childitem -Path $path -Include *password* -Recurse -ErrorAction SilentlyContinue | Export-csv $ExportPath -NoTypeInformation
